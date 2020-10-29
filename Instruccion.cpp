bf892f7e59cf4534e270b51b9d9750556303dd76
RiedelNicolas <nariedel99@gmail.com>
2020-10-29 12:33:41 -0300

    Cambio nombre de vertice por instruccion

--- Instruccion.cpp
+++ Instruccion.cpp
@@ -2,4 +2,4 @@
 // Created by riedel on 28/10/20.
 //
 
-#include "Vertice.h"
+#include "Instruccion.h"
--- Instruccion.h
+++ Instruccion.h
@@ -4,9 +4,13 @@
 
 #ifndef INC_7542_VERIFICADOR_EBPF_VERTICE_H
 #define INC_7542_VERIFICADOR_EBPF_VERTICE_H
+#include <string>
 
-
-class Vertice {
+class Instruccion {
+private:
+    int NumInstruccion;
+    std::string nombre;
+public:
 
 };
 
