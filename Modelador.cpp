8a8181fcc337492297eb31996feddb62f6189730
RiedelNicolas <nariedel99@gmail.com>
2020-10-30 18:01:45 -0300

    Implemento modelador

--- Modelador.cpp
+++ Modelador.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Modelador.h"
+
+Modelador::Modelador(std::string archivo) {
+    this->archivo.open( archivo.c_str() );
+    if( ! (this->archivo.is_open() ) ){
+        throw ("No se pudo abrir correctamente el archivo :"
+        + archivo);
+    }
+}
+
+int Modelador::cargarInstrucciones(Grafo &grafo) {
+    return 0;
+}
+
+int Modelador::relacionarInstrucciones(Grafo &grafo) {
+    return 0;
+}
+
+Modelador::~Modelador() {
+    this->archivo.close();
+}
+
--- Modelador.h
+++ Modelador.h
@@ -4,13 +4,18 @@
 
 #ifndef INC_7542_VERIFICADOR_EBPF_MODELADOR_H
 #define INC_7542_VERIFICADOR_EBPF_MODELADOR_H
-
+#include <fstream>
+#include "Grafo.h"
 
 class Modelador{
 private:
-
+std::ifstream archivo;
 public:
+    explicit Modelador(std::string archivo);
+    int cargarInstrucciones(Grafo& grafo);
+    int relacionarInstrucciones(Grafo& grafo);
 
+    ~Modelador();
 };
 
 
