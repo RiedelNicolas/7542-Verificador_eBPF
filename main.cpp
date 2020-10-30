537c645a3e41668644bbbd2f2fa2c2e75b1fd662
RiedelNicolas <nariedel99@gmail.com>
2020-10-29 23:19:31 -0300

    Nodos e intrucciones caminando

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-#include "inst"
+#include "Nodo.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    Instruccion instruccion("Jorge",5);
+    Nodo nodo1(instruccion);
+    Nodo nodo2(Instruccion("Mocoreta", 6))
+    nodo1.agregar_adyacente(&nodo2);
     return 0;
 }
