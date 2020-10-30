d0b4f7a93e9b7111f3fa5391f0f764479bed8b6b
RiedelNicolas <nariedel99@gmail.com>
2020-10-30 16:36:23 -0300

    Cambio de parseador a Modelador.cpp

--- Modelador.cpp
+++ Modelador.cpp
@@ -2,4 +2,4 @@
 // Created by riedel on 30/10/20.
 //
 
-#include "Parseador.h"
+#include "Modelador.h"
---
+++ Modelador.h
@@ -0,0 +1,17 @@
+//
+// Created by riedel on 30/10/20.
+//
+
+#ifndef INC_7542_VERIFICADOR_EBPF_MODELADOR_H
+#define INC_7542_VERIFICADOR_EBPF_MODELADOR_H
+
+
+class Modelador{
+private:
+
+public:
+
+};
+
+
+#endif //INC_7542_VERIFICADOR_EBPF_MODELADOR_H
