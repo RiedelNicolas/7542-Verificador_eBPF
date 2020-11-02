01608fbf840fc1f098573732bc8e0643944f95bc
RiedelNicolas <nariedel99@gmail.com>
2020-11-02 16:19:42 -0300

    Ciclos de DFS andando

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 #include <list>
 
 int main() {
+    bool ci;
     Grafo grafo;
     Modelador modelador;
     modelador.modelar(grafo,"test.txt");
-
+    ci = grafo.esCiclico();
+    std::cout << ci;
     return 0;
 
 }
