6c9eb35a9126e4d7b15f83607ba0603ce63d455b
RiedelNicolas <nariedel99@gmail.com>
2020-11-02 19:54:06 -0300

    Creo la clase Lock

---
+++ Lock.h
@@ -0,0 +1,14 @@
+//
+// Created by riedel on 2/11/20.
+//
+
+#ifndef INC_7542_VERIFICADOR_EBPF_LOCK_H
+#define INC_7542_VERIFICADOR_EBPF_LOCK_H
+
+
+class Lock {
+
+};
+
+
+#endif //INC_7542_VERIFICADOR_EBPF_LOCK_H
