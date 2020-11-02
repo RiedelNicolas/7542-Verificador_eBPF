3ad755d00f06dad5cb0547395b4383e3bd40dd9e
RiedelNicolas <nariedel99@gmail.com>
2020-11-02 19:02:45 -0300

    Creo clase thread.

---
+++ Thread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by riedel on 2/11/20.
+//
+
+#include "Thread.h"
---
+++ Thread.h
@@ -0,0 +1,14 @@
+//
+// Created by riedel on 2/11/20.
+//
+
+#ifndef INC_7542_VERIFICADOR_EBPF_THREAD_H
+#define INC_7542_VERIFICADOR_EBPF_THREAD_H
+
+
+class Thread {
+
+};
+
+
+#endif //INC_7542_VERIFICADOR_EBPF_THREAD_H
