f120f482e545be50e6a367eb94d38f0497070d36
RiedelNicolas <nariedel99@gmail.com>
2020-11-03 02:47:12 -0300

    cpplint

--- BuzonResultados.h
+++ BuzonResultados.h
@@ -2,26 +2,25 @@
 // Created by riedel on 2/11/20.
 //
 
-#ifndef INC_7542_VERIFICADOR_EBPF_BUZONRESULTADOS_H
-#define INC_7542_VERIFICADOR_EBPF_BUZONRESULTADOS_H
+#ifndef BUZONRESULTADOS_H_
+#define BUZONRESULTADOS_H_
 #include <string>
 #include <list>
 #include "Bloquear.h"
 
 class BuzonResultados {
-private:
+ private:
     std::list<std::string> lista;
     std::mutex m;
     void ordenar();
     void agregarResultado(std::string archivo,
                                            std::string mensaje);
-public:
+ public:
     void agregarConBucle(std::string path);
     void agregarSinUsar(std::string path);
     void agregarExitoso(std::string path);
     void MostrarResultados();
-
 };
 
 
-#endif //INC_7542_VERIFICADOR_EBPF_BUZONRESULTADOS_H
+#endif  // BUZONRESULTADOS_H_
