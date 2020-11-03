6d0547d4230c35cf53fa673cf20f65b7127c476b
RiedelNicolas <nariedel99@gmail.com>
2020-11-03 03:14:14 -0300

    cpplint

--- Modelador.h
+++ Modelador.h
@@ -2,25 +2,25 @@
 // Created by riedel on 30/10/20.
 //
 
-#ifndef INC_7542_VERIFICADOR_EBPF_MODELADOR_H
-#define INC_7542_VERIFICADOR_EBPF_MODELADOR_H
+#ifndef MODELADOR_H_
+#define MODELADOR_H_
 #include "Grafo.h"
 #include "Parseador.h"
 #include "Instruccion.h"
+#include <string>
 #include <list>
 
 class Modelador{
-
-private:
+ private:
     std:: list<Instruccion> instrucciones;
     Parseador parseador;
     void cargarInstrucciones(Grafo& grafo);
     void relacionarInstrucciones(Grafo& grafo);
     int parsearInstrucciones(std::string path);
-public:
+ public:
     int modelar(Grafo& grafo, std::string path);
     ~Modelador();
 };
 
 
-#endif //INC_7542_VERIFICADOR_EBPF_MODELADOR_H
+#endif  // MODELADOR_H_
--- Nodo.h
+++ Nodo.h
@@ -2,17 +2,17 @@
 // Created by riedel on 29/10/20.
 //
 
-#ifndef INC_7542_VERIFICADOR_EBPF_NODO_H
-#define INC_7542_VERIFICADOR_EBPF_NODO_H
+#ifndef NODO_H_
+#define NODO_H_
 #include <string>
 #include <list>
 
 class Nodo {
-private:
+ private:
     int id;
     std::string label;
-    std::list<Nodo*> adyacentes; //No se puede usar containers a referencias.
-public:
+    std::list<Nodo*> adyacentes;  // No se puede usar containers a referencias.
+ public:
     explicit Nodo(const int& id);
     explicit Nodo(const int& id, const std::string& label);
     const std::list<Nodo*>& getAdyacentes();
@@ -22,4 +22,4 @@ public:
 };
 
 
-#endif //INC_7542_VERIFICADOR_EBPF_NODO_H
+#endif  // NODO_H_
