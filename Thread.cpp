59232476254a463a356d16e252d01f84dcdc03b3
RiedelNicolas <nariedel99@gmail.com>
2020-11-02 21:14:20 -0300

    Implemento la clase Thread.

--- Thread.cpp
+++ Thread.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Thread.h"
+
+void Thread::iniciar() {
+    this->thread = std::thread(&Thread::run, this);
+}
+
+void Thread::join() {
+    thread.join();
+}
+
+Thread::Thread(Thread &&recibido) {
+    this->thread = std::move(recibido.thread);
+}
+
+Thread &Thread::operator=(Thread &&recibido) {
+        this->thread = std::move(recibido.thread);
+        return *this;
+}
+
+
--- Thread.h
+++ Thread.h
@@ -6,7 +6,24 @@
 #define INC_7542_VERIFICADOR_EBPF_THREAD_H
 
 
+#include <thread>
+
 class Thread {
+private:
+    std::thread thread;
+public:
+    Thread () = default ;
+    void iniciar();
+    void join();
+    virtual void run() = 0; // la tienen que implementar los hijos.
+    virtual ~Thread() = default;
+
+    Thread(const Thread&) = delete;
+    Thread& operator=(const Thread&) = delete;
+
+    Thread(Thread&& other);
+
+    Thread& operator=(Thread&& other);
 
 };
 
