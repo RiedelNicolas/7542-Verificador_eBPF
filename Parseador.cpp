e0fc93fa7941ffd9ef409f0b962cfc39afcefb07
RiedelNicolas <nariedel99@gmail.com>
2020-11-03 03:17:34 -0300

    cpplint

--- Parseador.cpp
+++ Parseador.cpp
@@ -2,6 +2,7 @@
 // Created by riedel on 31/10/20.
 //
 
+#include <vector>
 #include <sstream>
 #include <algorithm>
 #include "Parseador.h"
@@ -10,9 +11,9 @@ void Parseador::parsearInstrucciones(std::ifstream& archivo,
                                      std::list<Instruccion> &instrucciones) {
     this->iteracion = 0;
     std::string linea;
-    while (std::getline(archivo,linea )) {
-        if( linea.empty() ) continue;
-        instrucciones.push_back(parsearInstruccion(linea) ) ;
+    while (std::getline(archivo, linea )) {
+        if ( linea.empty() ) continue;
+        instrucciones.push_back(parsearInstruccion(linea) );
         this->iteracion++;
     }
 }
@@ -23,20 +24,20 @@ Instruccion Parseador::parsearInstruccion(std::string &linea) {
     std::string aux;
     size_t pos, fin;
     pos = linea.find(this->finDeLabel);
-    if(pos != std::string::npos ){
-        aux = linea.substr(0,pos);
-        linea.erase(0,pos+1);
+    if ( pos != std::string::npos ) {
+        aux = linea.substr(0, pos);
+        linea.erase(0, pos+1);
     }
     instruccion.setEtiqueta(aux);
     pos = linea.find_first_not_of(' ');
-    fin = linea.find_first_of(' ',pos);
+    fin = linea.find_first_of(' ', pos);
     aux = linea.substr(pos, fin-pos);
     instruccion.setIdentificador(aux);
     linea.erase(pos, aux.size());
     pos = linea.find_first_not_of(' ');
-    linea.erase(0,pos);
-    std::stringstream s (linea);
-    while(s.good() ){
+    linea.erase(0, pos);
+    std::stringstream s(linea);
+    while ( s.good() ) {
         getline(s, aux, ',');
        aux.erase(std::remove_if(aux.begin(), aux.end(), ::isspace), aux.end());
         args.push_back(aux);
@@ -58,25 +59,7 @@ const std::string Parseador::encontrarLabel(const std::string &linea) {
 */
 Parseador::Parseador() :finDeLabel(':'),
                         delimitador(' '),
-                        finArg(','){
+                        finArg(',') {
     this->iteracion = 0;
 }
-/*
 
-
-const std::string Parseador::encontrarInstruccion(const std::string &linea) {
-
-    std::string ins;
-    size_t pos_label,pos_ins;
-    pos_label = linea.find(this->finDeLabel);
-    if( pos_label == std::string::npos ){
-        pos_label = 0;
-    }
-    ins = linea.substr(pos_label);
-    pos_ins = ins.find_first_not_of(' ');
-    ins = ins.substr(pos_ins,
-                     ins.find_first_of(' '
-            ,pos_ins));
-    return ins;
-}
-*/
