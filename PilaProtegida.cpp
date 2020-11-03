6393410db51ddea9baad1e75675956f94e9b65e0
RiedelNicolas <nariedel99@gmail.com>
2020-11-02 22:27:28 -0300

    Pila protegida andando

--- PilaProtegida.cpp
+++ PilaProtegida.cpp
@@ -4,6 +4,19 @@
 
 #include "PilaProtegida.h"
 
-void PilaProtegida::apilar(std::string) {
+void PilaProtegida::apilar(std::string recibido) {
+    Bloquear b (this->m);
+    this->pila.push(recibido);
+}
 
+std::string PilaProtegida::desapilar() {
+    Bloquear b (this->m);
+    std::string top = this->pila.top();
+    this->pila.pop();
+    return top;
+}
+
+bool PilaProtegida::estaVacia() {
+    Bloquear b (this->m);
+    return this->pila.empty();
 }
